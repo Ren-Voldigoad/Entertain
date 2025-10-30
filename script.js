0655d6296624986b6c739243aaf3ae99834e8b54
Ren-Voldigoad <narendrabayupermadi623@gmail.com>
2025-10-30 11:13:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-pilih tombol berdasarkan ID-nya
+
 const tombolSapa = document gentElementById('SapaButton')
-tambahkan 'event listener'untuk aksi 'click'tombolSapa.addEventlistener('click',function() {
-  jalankan kode ini ketika tombol diklik
+'event listener''click'tombolSapa.addEventlistener('click',function() {
   alert('Halo! Terima kasih sudah berkunjung!')}):
