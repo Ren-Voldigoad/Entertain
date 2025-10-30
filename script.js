63946e8e061e4c466cb39a74f6b2d8a4158b03ad
Ren-Voldigoad <narendrabayupermadi623@gmail.com>
2025-10-30 11:13:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 
-const tombolSapa = document gentElementById('SapaButton')
+const tombolSapa = document getElementById('SapaButton')
 'event listener''click'tombolSapa.addEventlistener('click',function() {
   alert('Halo! Terima kasih sudah berkunjung!')}):
